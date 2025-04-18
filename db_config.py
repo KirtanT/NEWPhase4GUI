e6de83e93c0c0a317dce0e1a62ab732a8620346d
Kirtan Thakkar <kirtanhthakkar@gmail.com>
2025-04-17 21:14:01 -0400

    password comment

--- db_config.py
+++ db_config.py
@@ -5,6 +5,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="", #PUT YOUR PASSWORD HERE
         database="flight_tracking"
     )
