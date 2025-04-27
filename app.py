01e3fe41a8541feb742b190899f22cacca71cf0a
gkrishnan05 <gautamkrishnan05@gmail.com>
2025-04-26 22:20:10 -0400

    some more minor changes (adding comments)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 from db_config import get_db_connection
 import datetime
 
@@ -8,10 +8,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# create funciton for stored procedures by accessing flight_tracking database
-def pcall(procedure_name, inputs):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+# route the procedure call back to the html doc
+@app.route('/procedure', methods=['POST'])
+def prun():
+    data = request.get_json()
+    procedure_name = data.get('procedure_name')     #grabbing the procedure inputs to input into DB
+    inputs = data.get('inputs', [])
+
+    # error catching
+    try:
+        results = pcall(procedure_name, inputs)
+        return results
+    except KeyError as ke:
+        return jsonify({'error': f'Missing required field: {str(ke)}'}), 400
+    except ValueError as ve:
+        return jsonify({'error': {str(ke)}}), 400
+    except Exception as e:
+        return jsonify({f"Unexpected error calling procedure {procedure_name}: {str(e)}"}), 500
+    
+# create funciton for stored procedures by accessing flight tracking DB
+def pcall(procedure_name, inputs):      # using inputs from prun
+    connection = get_db_connection()
+    cursor = connection.cursor()
     try:
         cursor.callproc(procedure_name, inputs)
         results = []
@@ -19,33 +37,40 @@ def pcall(procedure_name, inputs):
             cols = [d[0] for d in result.description]
             for row in result.fetchall():
                 results.append(dict(zip(cols, row)))
-        conn.commit()
+        connection.commit()
         return results
     finally:
         cursor.close()
-        conn.close()
+        connection.close()
 
-# route the procedure call back to the html doc
-@app.route('/procedure', methods=['POST'])
-def prun():
-    data = request.get_json()
-    procedure_name = data.get('procedure_name')
-    inputs = data.get('inputs', [])
+@app.route('/view/<view>', methods=['GET'])
+def vrun(view):
+    # making sure view exists
+    if view not in {
+        'flights_in_the_air', 'flights_on_the_ground',
+        'people_in_the_air', 'people_on_the_ground',
+        'route_summary', 'alternative_airports'}:
+        return 'View not found', 404
+    # catching any errors
     try:
-        results = pcall(procedure_name, inputs)
-        return results
+        data = vcall(view)
+        return jsonify(data)
+    except ValueError as ve:
+        return jsonify({f"Bad request on {view}: {ve}"}), 400
     except Exception as e:
-        return str(e), 500
-
-# view endpoint
+        return jsonify({f"Unexpected server error when fetching view {view}: {e}"}), 500
+    
+# create function for view call using input from vrun
 def vcall(view):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    # put it in a try block to make sure there aren't unexpected errors
     try:
         cursor.execute(f"select * from {view}")
-        cols = [d[0] for d in cursor.description]
-        rows = cursor.fetchall()
+        cols = [d[0] for d in cursor.description]   # grabbing columns from DB
+        rows = cursor.fetchall()        # grabbing rows from DB
         output = []
+        # turning the DB table into dict format
         for row in rows:
             rec = {}
             for col, val in zip(cols, row):
@@ -59,20 +84,8 @@ def vcall(view):
         return output
     finally:
         cursor.close()
-        conn.close()
+        connection.close()
 
-@app.route('/view/<view>', methods=['GET'])
-def vrun(view):
-    if view not in {
-        'flights_in_the_air', 'flights_on_the_ground',
-        'people_in_the_air', 'people_on_the_ground',
-        'route_summary', 'alternative_airports'}:
-        return 'View not found', 404
-    try:
-        data = vcall(view)
-        return data
-    except Exception as e:
-        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db_config.py
+++ db_config.py
@@ -5,5 +5,5 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="gk789.mysql", #PUT YOUR PASSWORD HERE
+        password="", #PUT YOUR PASSWORD HERE
         database="flight_tracking")
