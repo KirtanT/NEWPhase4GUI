0febe2447b6a879df622d6ad3501ad70c8790536
gkrishnan05 <gautamkrishnan05@gmail.com>
2025-04-26 22:21:29 -0400

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -5,5 +5,12 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
+<<<<<<< Updated upstream
         password="", #PUT YOUR PASSWORD HERE
         database="flight_tracking")
+=======
+        password="gk789.mysql", #PUT YOUR PASSWORD HERE
+        database="flight_tracking"
+
+    )
+>>>>>>> Stashed changes
