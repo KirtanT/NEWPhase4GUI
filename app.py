6014dc8cd9e52826ab983c86853b981bfb58c37a
gkrishnan05 <gautamkrishnan05@gmail.com>
2025-04-27 15:07:56 -0400

    some minor changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import datetime
 
 app = Flask(__name__)
 
+# linking flask to html site
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,21 +15,19 @@ def prun():
     data = request.get_json()
     procedure_name = data.get('procedure_name')     #grabbing the procedure inputs to input into DB
     inputs = data.get('inputs', [])
-
-    # error catching
     try:
         results = pcall(procedure_name, inputs)
-        return results
-    except KeyError as ke:
-        return jsonify({'error': f'Missing required field: {str(ke)}'}), 400
-    except ValueError as ve:
-        return jsonify({'error': {str(ke)}}), 400
-    except Exception as e:
-        return jsonify({f"Unexpected error calling procedure {procedure_name}: {str(e)}"}), 500
+        return jsonify(results)
+    except KeyError as key:
+        return jsonify({'error': 'Primary key cannot be null: ' + str(key)}), 400
+    except ValueError as value:
+        return jsonify({'error': {str(value)}}), 400
+    except Exception as exc:
+        return jsonify({"Error calling" + str(procedure_name) + ':' + str(exc)}), 500
     
 # create funciton for stored procedures by accessing flight tracking DB
-def pcall(procedure_name, inputs):      # using inputs from prun
-    connection = get_db_connection()
+def pcall(procedure_name, inputs):
+    connection = get_db_connection()    # linking inputs to the DB in db_config.py
     cursor = connection.cursor()
     try:
         cursor.callproc(procedure_name, inputs)
@@ -45,36 +44,30 @@ def pcall(procedure_name, inputs):      # using inputs from prun
 
 @app.route('/view/<view>', methods=['GET'])
 def vrun(view):
-    # making sure view exists
-    if view not in {
-        'flights_in_the_air', 'flights_on_the_ground',
-        'people_in_the_air', 'people_on_the_ground',
-        'route_summary', 'alternative_airports'}:
+    if view not in ['flights_in_the_air', 'flights_on_the_ground', 'people_in_the_air', 'people_on_the_ground', 'route_summary', 'alternative_airports']:
         return 'View not found', 404
-    # catching any errors
     try:
         data = vcall(view)
         return jsonify(data)
-    except ValueError as ve:
-        return jsonify({f"Bad request on {view}: {ve}"}), 400
-    except Exception as e:
-        return jsonify({f"Unexpected server error when fetching view {view}: {e}"}), 500
+    except ValueError as value:
+        return jsonify({f"Bad request on {view}: {value}"}), 400
+    except Exception as exc:
+        return jsonify({f"Server Error fetching {view}: {exc}"}), 500
     
 # create function for view call using input from vrun
 def vcall(view):
-    connection = get_db_connection()
+    connection = get_db_connection()    # linking inputs to the DB in db_config.py
     cursor = connection.cursor()
-    # put it in a try block to make sure there aren't unexpected errors
     try:
         cursor.execute(f"select * from {view}")
-        cols = [d[0] for d in cursor.description]   # grabbing columns from DB
-        rows = cursor.fetchall()        # grabbing rows from DB
+        cols = [d[0] for d in cursor.description]
+        rows = cursor.fetchall()
         output = []
         # turning the DB table into dict format
         for row in rows:
             rec = {}
             for col, val in zip(cols, row):
-                if isinstance(val, datetime.timedelta):
+                if isinstance(val, datetime.timedelta):    # making sure to avoid unserializable object in JSON
                     rec[col] = str(val)
                 elif isinstance(val, (datetime.date, datetime.time, datetime.datetime)):
                     rec[col] = val.isoformat()
